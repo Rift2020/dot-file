3e239f1d6181e4e89e2ca39dfef9c3df51744d3b
Rift <rifts@tutanota.com>
2023-09-02 16:15:52 +0800

    add nixos-config

---
+++ nixos/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Rift's NixOS Flake";
+
+    nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.cernet.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  # 这是 flake.nix 的标准格式，inputs 是 flake 的依赖，outputs 是 flake 的输出
+  # inputs 中的每一项依赖都会在被拉取、构建后，作为参数传递给 outputs 函数
+  inputs = {
+    # flake inputs 有很多种引用方式，应用最广泛的格式是:
+    #     github:owner/name/reference
+    # 即 github 仓库地址 + branch/commit-id/tag
+
+    # NixOS 官方软件源，这里使用 nixos-unstable 分支
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # home-manager，用于管理用户配置
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # 这里的 nixos-test 替换成你的主机名称
+      rift-dear-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          # 将 home-manager 配置为 nixos 的一个 module
+          # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.rift = import ./home.nix;
+
+            # 使用 home-manager.extraSpecialArgs 自定义传递给 ./home.nix 的参数
+            # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
+            # home-manager.extraSpecialArgs = inputs;
+          }
+        ];
+      };
+    };
+  };
+}
