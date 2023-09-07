6d543273fe8ccc7ccad9699649b7d3751c864b23
Rift <rifts@tutanota.com>
2023-09-07 11:14:50 +0800

    enable treesitter, add comment plug, use catppuccin color

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,43 +1,59 @@
+--recommend install in cli: 
+-- tree-sitter
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd [[packadd packer.nvim]]
 end
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-  -- Packer can manage itself
+	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
-	
+
+	use { "catppuccin/nvim", as = "catppuccin" }
+	-- use 'tanvirtin/monokai.nvim'
+
 	use 'kyazdani42/nvim-tree.lua'
-	
+
 	use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
 
 	use {'neoclide/coc.nvim', branch = 'release'}
 	use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
+		'nvim-treesitter/nvim-treesitter',
+		run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+	}
+
 	use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
 	})
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup()
+		end
+	}
 
 	use {'ojroques/vim-oscyank',branch = 'main'}
+
 	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 	if packer_bootstrap then
 		require('packer').sync()
 	end
 end)
 
 require("config.nvim-tree")
+require("config.nvim-treesitter")
 require('lualine').setup{options = { icons_enabled = false,theme = 'ayu_mirage' }}
+require('config.catppuccin')
